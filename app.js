63b3a204eb159dfbffd68e0cf47a103b40312743
alu0100785265 <alu0100785265@ull.edu.es>
2017-03-12 18:35:03 +0000

    cambios

--- app.js
+++ app.js
@@ -10,13 +10,13 @@ var proces = require('child_process');
 
 
 
-app.set('port', (process.env.PORT || 8080));
-app.use(express.static('gh-pages'));
+app.set('port', (process.env.PORT || 5000));
+app.use(express.static('_book'));
 
 
 
 app.get('/', function(request, response){
-  response.render('index');  
+  response.sendfile('index');  
   
 });
 
